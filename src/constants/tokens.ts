d1bd5323a07acb1163798f0a4da9a53ffb84ef63
andresdanielmtz <autummata@gmail.com>
2026-04-25 16:03:25 -0600

    update: token_type, token

--- src/constants/tokens.ts
+++ src/constants/tokens.ts
@@ -1,7 +1,7 @@
 /**
  * This file defines the various token types that the lexer will recognize in the source code.
  */
-enum TOKENS {
+enum TOKEN_TYPE {
     IDENTIFIER = "IDENTIFIER",
     NUMBER = "NUMBER",
     STRING = "STRING",
@@ -13,4 +13,11 @@ enum TOKENS {
     EOF = "EOF"   
 }
 
-export { TOKENS };
+interface Token { 
+    type: TOKEN_TYPE;
+    value: string;
+    line: number;
+    column: number;
+}
+
+export { TOKEN_TYPE, type Token };
